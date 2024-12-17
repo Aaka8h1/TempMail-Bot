a930efedd31b834c2506422d00d767b795aea0a3
Aaka8h1 <kanduaakash07@gmail.com>
2024-12-17 20:46:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@
 # Channel: https://t.me/itsSmartDev
 
 # Pyrogram setup
-API_ID = "12345678"  # Replace this API ID with your actual API ID
-API_HASH = "XXXXXXXXXXXX"  # Replace this API HASH with your actual API HASH
-BOT_TOKEN = "XXXXXXX:XXXXXXXX"  # Replace this BOT_TOKEN
+API_ID = "29199850"  # Replace this API ID with your actual API ID
+API_HASH = "711c12d4f9bb08a1342715862e4df9c9"  # Replace this API HASH with your actual API HASH
+BOT_TOKEN = "8156719453:AAGexHVAHzLVmMA8GqctLk8niuYW3InyAX8"  # Replace this BOT_TOKEN
